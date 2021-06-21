8f2223becbef90181868debb847ec76f12541405
Satish VIjayan <satish@charioteersoftware.com>
2021-06-21 11:45:03 +0530

    setup - try again

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
+# get version from __version__ variable in pivot_table/__init__.py
+from pivot_table import __version__ as version
+
 setup(
 	name='pivot_table',
 	version=version,
